931a5db35609b7d386154c26079e00e24052be7c
chanho <puk0806@naver.com>
2019-04-28 16:29:29 +0900

    3th commit thirddjangoproject_blog

--- thirdproject/settings.py
+++ thirdproject/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blogapp.apps.BlogappConfig',
+    'portfolio.apps.PortfolioConfig',
 ]
 
 MIDDLEWARE = [
@@ -119,3 +120,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# static 파일들이 현재 어디에 있는지 쓰는 곳
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'portfolio','static')
+]
+
+# static 파이들이 어디로 모일 것인지를 쓰는 곳
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
--- thirdproject/urls.py
+++ thirdproject/urls.py
@@ -1,11 +1,14 @@
 from django.contrib import admin
 from django.urls import path
 import blogapp.views
+import portfolio.views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', blogapp.views.home, name ="name"),
     path('blog/<int:blog_id>',blogapp.views.detail, name = "detail"),
-    path('blog/new/', blogapp.views.new , name = 'new'),
-    path('blog/create', blogapp.views.create, name = 'create'),
+    path('blog/new/', blogapp.views.new , name = "new"),
+    path('blog/create', blogapp.views.create, name = "create"),
+    path('portfolio/',portfolio.views.portfolio, name = "portfolio"),
 ]
