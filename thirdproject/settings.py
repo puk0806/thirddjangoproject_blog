436a1bc6273bfb59106dbd2e4b027b8798815b44
chanho <puk0806@naver.com>
2019-04-29 19:21:02 +0900

    5th commit thirddjangoproject_blog

--- thirdproject/settings.py
+++ thirdproject/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'thirdproject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['thirdproject/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
