c20d4b280403bf63ce8f16925cfe36a5db0f94bf
chanho <puk0806@naver.com>
2019-04-29 22:24:05 +0900

    6th commit thirddjangoproject_bloc

--- thirdproject/urls.py
+++ thirdproject/urls.py
@@ -2,14 +2,17 @@ from django.contrib import admin
 from django.urls import path, include
 import blogapp.views
 import portfolio.views
+import accounts.views
 from django.conf import settings
 from django.conf.urls.static import static
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', blogapp.views.home, name ="name"),
+    path('', blogapp.views.home, name ="home"),
     path('blog/', include('blogapp.urls')),
-    path('portfolio/',portfolio.views.portfolio, name = "portfolio"),
+    path('portfolio/', portfolio.views.portfolio, name = "portfolio"),
+    path('accounts/', include('accounts.urls')),
 ] # + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 # 위에 처럼 하던가 아래처럼 
